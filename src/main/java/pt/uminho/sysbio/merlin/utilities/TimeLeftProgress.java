5fc2ca773217a1c2ada65bf29e4b4cf762ddf744
Oscar Dias <odias@ceb.uminho.pt>
2016-08-26 18:07:41 +0100

    ebi webservices

--- src/main/java/pt/uminho/sysbio/merlin/utilities/TimeLeftProgress.java
+++ src/main/java/pt/uminho/sysbio/merlin/utilities/TimeLeftProgress.java
@@ -44,10 +44,63 @@ public class TimeLeftProgress extends OperationProgress{
 			int percentage = Math.round(((float)pos/(float)max)*100);
 			
 			
-			if(minutes>59) {
-				
+			if(minutes>59)				
 				minutes = minutes % 60;
+
+			int hour = (int) timeL/3600 ;
+//			System.out.println("pos :"+(float)pos);
+//			System.out.println("max :"+(float)max+"\n");
+//			System.out.println("progress :"+((float)pos/(float)max)+"\n");
+//			System.out.println("progress :"+((float)pos/(float)max)*100+"\n");
+//			System.out.println("timeRunSeconds :"+seconds);
+//			System.out.println("eachSeconds :"+eachSeconds);
+//			System.out.println("timeL :"+timeL);
+//			System.out.println("minutes :"+minutes);
+//			System.out.println("hour :"+hour);
+			//System.out.println(this.getProgress());
+			
+			if(minutes==0 && hour==0) {
+				
+				timeLeft = timeLeft.concat("Time Left : <1m");
 			}
+			else {
+				
+				timeLeft = timeLeft.concat(percentage+"%   Time Left : "+hour+"h "+minutes+"m");
+			}
+		}
+		else {
+			
+			timeLeft = timeLeft.concat("Time Left : oo");
+		}
+		this.time=timeLeft;
+	}
+	
+	/**
+	 * @param differTime
+	 * @param pos
+	 * @param max
+	 * @param message
+	 */
+	public void setTime(long differTime, int pos, int max, String message) {
+		
+		message = message.trim();
+		if(!message.isEmpty())
+			message+=" ";
+		
+		String timeLeft = new String(message);
+		
+		if(pos!=0) {
+			
+			float seconds = differTime/(float)1000;
+			float eachSeconds = seconds/(float)pos;
+			float timeL = eachSeconds*(max-pos); 
+			int minutes = (int)timeL/60;
+			this.setProgress(((float)pos/(float)max));
+			int percentage = Math.round(((float)pos/(float)max)*100);
+			
+			if(minutes>59)				
+				minutes = minutes % 60;
+
 			int hour = (int) timeL/3600 ;
 //			System.out.println("pos :"+(float)pos);
 //			System.out.println("max :"+(float)max+"\n");
